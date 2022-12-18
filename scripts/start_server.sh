f0e546a44014e540eb4ac8f2fc351904b85e8378
DungAT <dungat98@gmail.com>
2022-12-18 16:06:21 +0700

    Add release script

---
+++ scripts/start_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo starting server
+
+# here we just use npm to run the build
+cd /var/www/
+echo building application...
+sudo npm run build
+
+# start the application with pm2
+echo starting application...
+sudo pm2 serve build 80 --name "learning-react" --spa
