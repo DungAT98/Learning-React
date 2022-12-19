2b3aaacd3e700f7806550b6bc2a0115119dac570
DungAT <dungat98@gmail.com>
2022-12-19 10:03:43 +0700

    Resolve pm2 error command

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 # this file runs first, so during an initail installation error might happen
 
 # simply stop the application process using pm2
